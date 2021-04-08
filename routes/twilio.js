d25c5a8e182e11cecd87a2795f0b19fbf9d9693e
leightonchien <leightonchien@gmail.com>
2021-04-08 01:57:23 +0000

    new token assigned to run locally

--- routes/twilio.js
+++ routes/twilio.js
@@ -1,36 +1,37 @@
-// const MessagingResponse = require('twilio').twiml.MessagingResponse;
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
-// const accountSid = 'AC6624ad3f551c59ad2a5c2b6f92af56bd';
-// const authToken = 'replace new token';
-// const client = require('twilio')(accountSid, authToken);
+const accountSid = 'AC6624ad3f551c59ad2a5c2b6f92af56bd';
+const authToken = 'add new token here';
+const client = require('twilio')(accountSid, authToken);
 
 
 
-// const express = require('express');
-// const router  = express.Router();
+const express = require('express');
+const router  = express.Router();
 
-// module.exports = (db) => {
-//   router.get("/", (req, res) => {
-//     console.log("hello world users.route")
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+    console.log("hello world users.route")
     
-//     console.log("about to call twilio")
+    console.log("about to call twilio")
 
          
-//     client.messages 
-//           .create({ 
-//              body: 'Your bubble tea 🥤 will be ready in 15 minutes! ',  
-//              messagingServiceSid: 'MG496ea940eb8cc83b39ab8267aef2ccf9',      
-//              to: '+17788956372' 
-//            }) 
-        
-//           .then(message => console.log(message.sid)) 
-//           .done();
+    client.messages 
+          .create({ 
+             body: 'Your bubble tea 🥤 will be ready in 15 minutes! ',  
+             messagingServiceSid: 'MG496ea940eb8cc83b39ab8267aef2ccf9',      
+             to: '+17788956372' 
+           }) 
+          .then(message => console.log(message.sid)) 
+          .done();
 
-//     console.log("twilio has been called")
+    console.log("twilio has been called")
 
 
-//   });
-//   return router;
-// };
+
+
+  });
+  return router;
+};
 
 
